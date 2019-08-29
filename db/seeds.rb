43e47c5dce841990e54fb65ef995bb67384499f7
fromwhence <wenc.matt@gmail.com>
2019-08-29 10:34:04 -0500

    product creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(
+                      name: "Leica M3",
+                      price: 2500,
+                      image_url: "https://kenrockwell.com/leica/m3/D3S_7742-50mm-f14.jpg",
+                      description: "The LEICA M3 is the best camera that LEICA has ever made, and by many accounts, the best camera of all time. This is why it's also LEICA's best-selling camera of all time."
+                      )
+contact.save
+
+product = Product.new(
+                      name: "Contax T",
+                      price: 1200,
+                      image_url: "https://www.kenrockwell.com/contax/t/D3S_9401-1200.jpg",
+                      description: "The CONTAX T is a very high-quality compact 35mm camera. It is a rangefinder manual focus, manual-wind camera with an automatic electronic leaf shutter."
+                      )
+contact.save
+
+product = Product.new(
+                      name: "Canon AE-1",
+                      price:, "150"
+                      image_url:, "https://kenrockwell.com/canon/fd/images/ae-1-program/D3S_7445-1200.jpg"
+                      description: "The Canon AE-1 Program is one of the most popular cameras of all time, and for very good reason: it is a spectacular camera."
+                      )
+contact.save
+
+
+
