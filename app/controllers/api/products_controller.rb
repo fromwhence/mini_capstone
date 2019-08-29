aae011f5fe13eb30a3e3443805a7729905a9d566
fromwhence <wenc.matt@gmail.com>
2019-08-29 17:08:23 -0500

    end of day commit

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,12 @@
 class Api::ProductsController < ApplicationController
-  def all_products_action
+  def all_products
     @products = Product.all
     render 'all_products_view.json.jb'
   end
+
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/all_products_url' => 'products#all_products_action'
+    get '/all_products' => 'products#all_products'
+    get '/first_product' => 'products#first_product'
   end
 end
