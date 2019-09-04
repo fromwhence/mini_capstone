85ceacef1642b562d4932d6b77c311268b13494e
fromwhence <wenc.matt@gmail.com>
2019-09-04 17:03:40 -0500

    end of day

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    price < 200 # if and result implied
+    #price < 200 ? true : false # ternary if statement
+    # if price < 150
+    #   true
+    # else
+    #   false
+    # end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax  
+  end
+
 end
+
